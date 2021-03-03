ee28032c803af40cdf0b124c4f846e8fd48a98f7
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-03-03 17:16:27 -0500

    chore: add standard tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb",
+  env: {
+    node: true,
+  },
+  root: true,
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 };
